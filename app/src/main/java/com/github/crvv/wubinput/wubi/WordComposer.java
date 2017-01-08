ff4b89a90def45f0b9d50b7abf2039ffd5d1331f
crvv <crvv.pku@gmail.com>
2017-01-08 22:59:22 +0800

    clear input when got fifth letter

--- app/src/main/java/com/github/crvv/wubinput/wubi/WordComposer.java
+++ app/src/main/java/com/github/crvv/wubinput/wubi/WordComposer.java
@@ -161,6 +161,9 @@ public final class WordComposer {
      * @param event the event to apply. Must not be null.
      */
     public void applyProcessedEvent(final Event event) {
+        if (size() >= MAX_WORD_LENGTH && Character.isLetter(event.mCodePoint)) {
+            reset();
+        }
         mCombinerChain.applyProcessedEvent(event);
         final int primaryCode = event.mCodePoint;
         final int keyX = event.mX;
